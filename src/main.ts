e6a425ac30a4cd42d4c30a2626332d2854f83e02
Gustavo A <gustavo.atencio@outlook.com>
2022-12-20 06:39:13 -0500

    get input

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ async function main() {
 	const browser = await chromium.launch({ headless: false })
 	const page = await browser.newPage()
 	await page.goto(TARGET_WEB)
-
+	const userLoginInput = await page.$("#user_login")
+	console.log({ userLoginInput })
 	await browser.close()
 }
 
