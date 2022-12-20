964b4e74c4f9c3532ed1770cc415747cb6639765
Gustavo A <gustavo.atencio@outlook.com>
2022-12-20 06:49:33 -0500

    set input value

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import { chromium } from "playwright"
+import { sleep } from "./utils"
 import { TARGET_WEB } from "./config"
 
 async function main() {
 	const browser = await chromium.launch({ headless: false })
 	const page = await browser.newPage()
 	await page.goto(TARGET_WEB)
-	const userLoginInput = await page.$("#user_login")
+	const userLoginInput = await page.$eval("#user_login",
+		(target: HTMLInputElement) => {
+			target.value = "test"
+			return target.value
+		}
+	)
 	console.log({ userLoginInput })
+	await sleep(1000)
 	await browser.close()
 }
 
