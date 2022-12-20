f1c9be05d70c4c9fa3553084a182a83397576e13
Gustavo A <gustavo.atencio@outlook.com>
2022-12-20 06:34:36 -0500

    open target web

---
+++ src/config.ts
@@ -0,0 +1,2 @@
+export const TARGET_WEB = process.env.TARGET_WEB || ""
+export const OBS_WEB_SOCKET_PASS = process.env.OBS_WEB_SOCKET_PASS || ""
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { chromium } from "playwright"
+import { TARGET_WEB } from "./config"
 
 async function main() {
 	const browser = await chromium.launch({ headless: false })
 	const page = await browser.newPage()
-	await page.goto("https://www.google.com")
-	await page.screenshot({ path: "./screenshots/google.png" })
+	await page.goto(TARGET_WEB)
+
 	await browser.close()
 }
 
