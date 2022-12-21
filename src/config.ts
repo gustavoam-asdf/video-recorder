af822bae107eb61108dfeebd30cb7ba972755489
Gustavo A <gustavo.atencio@outlook.com>
2022-12-20 22:42:13 -0500

    set input values

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,4 @@
 export const TARGET_WEB = process.env.TARGET_WEB || ""
 export const OBS_WEB_SOCKET_PASS = process.env.OBS_WEB_SOCKET_PASS || ""
+export const USER_NAME = process.env.USER_NAME || ""
+export const USER_PASS = process.env.USER_PASS || ""
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,29 @@
+import { TARGET_WEB, USER_NAME, USER_PASS } from "./config"
+
 import { chromium } from "playwright"
 import { sleep } from "./utils"
-import { TARGET_WEB } from "./config"
 
 async function main() {
-	const browser = await chromium.launch({ headless: false })
-	const page = await browser.newPage()
+	const browser = await chromium.launch({
+		headless: false
+	})
+	const context = await browser.newContext()
+	const page = await context.newPage()
+
 	await page.goto(TARGET_WEB)
-	const userLoginInput = await page.$eval("#user_login",
+	await page.$eval("#user_login",
 		(target: HTMLInputElement) => {
-			target.value = "test"
+			target.value = USER_NAME
 			return target.value
 		}
 	)
-	console.log({ userLoginInput })
-	await sleep(1000)
+	await page.$eval("#user_pass",
+		(target: HTMLInputElement) => {
+			target.value = USER_PASS
+			return target.value
+		}
+	)
+	await sleep(10000)
 	await browser.close()
 }
 
