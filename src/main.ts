57911c91ee4355f685a79ede317af3b716066eed
Gustavo A <gustavo.atencio@outlook.com>
2022-12-21 14:04:43 -0500

    listen request finished

--- src/main.ts
+++ src/main.ts
@@ -19,12 +19,28 @@ async function main() {
 
 	await page.goto(WEB_TARGET)
 
-	await page.$eval("div.post-content > div > div", (videoContainer: HTMLDivElement) => {
-		const videoCards = videoContainer.querySelectorAll("div >  div > a")
-		console.log(videoCards)
+	page.on("requestfinished", async request => {
+		if (!request.url().includes("player.vimeo.com/video")) return
+		const videoFrame = page.frames().find(frame => frame.url().includes("player.vimeo.com/video"))
+		if (!videoFrame) return
+		videoFrame.$$eval("button", (buttons) => {
+			console.log({ buttons })
+		})
+		await sleep(10000)
+		await page.mouse.click(500, 500)
 	})
 
-	await sleep(10000)
+	await page.$eval("div.post-content > div > div", (videoContainer: HTMLDivElement) => {
+		const videoCards = videoContainer.querySelectorAll("div >  div > a")
+		const videoCard = videoCards[0] as unknown as HTMLAnchorElement
+		videoCard.click()
+		return videoCards
+	}).then((videoCards) => {
+		// console.log({ videoCards })
+		console.log("Video card clicked")
+	})
+
+	await sleep(50000)
 	await browser.close()
 }
 
